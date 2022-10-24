a4ec119e5aa95ceca8da163e678db223c2fdc10a
Your Name <jankotan@gmail.com>
2022-10-24 11:37:00 +0200

    update setup

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ install_requires = [
     'numpy>1.6.0',
     'lxml',
     'fabio',
-    'nxstools',  # '>=3.16.2'
+    'nxstools',  # '>=3.30.0'
     # 'pymysqldb',
     # 'psycopg2-binary'
     # 'psycopg2'
