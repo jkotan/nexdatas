1e1848fcdb98516749c35d0f3a849f1a5cb0cf75
Jan Kotanski <jankotan@gmail.com>
2023-05-18 16:45:27 +0200

    update ELinkH5CPP tests

--- test/ELinkH5Cpp_test.py
+++ test/ELinkH5Cpp_test.py
@@ -264,7 +264,9 @@ class ELinkH5CppTest(unittest.TestCase):
         self.assertEqual(li6.h5Object, None)
         self.assertTrue(isinstance(li7.h5Object, H5CppWriter.H5CppLink))
         self.assertEqual(li7.h5Object.name, atts7["name"])
-        self.assertTrue(str(li7.h5Object.target_path).endswith(atts7["path"]))
+        # self.assertTrue(str(li7.h5Object.target_path).endswith(atts7["path"]))
+        self.assertTrue(str(li7.h5Object.target_path).replace(":NXentry", "").
+                        endswith(atts7["path"].replace(":NXentry", "")))
 
         l1 = self._nxFile.open("link1")
         self.assertEqual(l1.read(), fi2.h5Object.read())
